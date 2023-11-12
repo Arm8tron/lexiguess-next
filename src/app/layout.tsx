a7a82166a5e67a03e7b6d56f7977eceb9b245390
arm8tron <anirudh.makuluri@gmail.com>
2023-11-12 23:06:16 +0530

    Redeploy analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,21 +6,21 @@ import { Analytics } from '@vercel/analytics/react';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'LexiGuess',
-  description: 'Guess the word!',
+	title: 'LexiGuess',
+	description: 'Guess the word!',
 }
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body suppressHydrationWarning={true} className={`${inter.className} bg-primary-bg text-slate-100`}>
-		{children}
-		<Analytics/>
-		</body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<body suppressHydrationWarning={true} className={`${inter.className} bg-primary-bg text-slate-100`}>
+				{children}
+				<Analytics />
+			</body>
+		</html>
+	)
 }
